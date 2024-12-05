12c314d961d706c9fda0638f7a7b9d76ff64b904
Roland Nilsson <roland@minerva.net>
2024-12-05 14:01:30 +0100

    Use computed pool sizes and uptake/release

---
+++ common.py
@@ -0,0 +1,64 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from scipy.stats import norm
+import sys
+
+#  path to simpleflux package 
+simpleflux_path = 'C:/code/python/simpleflux/src'
+sys.path.append(simpleflux_path)
+
+from simpleflux.model import FluxModel, FluxState
+from simpleflux.modelstate import ModelState
+from simpleflux.fitting import ModelFit
+
+
+input_data_path = Path('00_input_data')
+derived_data_path = Path('02_heavy_fractions_concentrations')
+plots_path = Path('plots')
+
+# make matplotlib use TrueType fonts for PDF export
+matplotlib.rcParams['pdf.fonttype'] = 42
+
+# numpy default print settings
+np.set_printoptions(precision=3, suppress=True)
+
+
+## convenient functions
+
+def prepend_index_level(df: pd.DataFrame, values, name: str) -> pd.DataFrame:
+    return df.assign(**{name: values}).set_index(name, append=True).swaplevel()
+
+
+def plot_model_fit(flux_model: FluxModel, x_predicted: pd.DataFrame,
+                   heavy_fractions: pd.DataFrame, use_log_scale: bool = False) -> None:
+    y_axis_margin = 0.05
+    fig, axs = plt.subplots(ncols=len(flux_model.metabolites), figsize=(15,3))
+    for i, metabolite in enumerate(flux_model.metabolites):
+        axs[i].plot(x_predicted[metabolite])
+        if metabolite in heavy_fractions.columns:
+            axs[i].plot(heavy_fractions[metabolite], '.')
+        axs[i].set_ylim(-y_axis_margin, 1 + y_axis_margin)
+        axs[i].set_title(metabolite)
+        if use_log_scale:
+            axs[i].set_xscale('log')
+            axs[i].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+        axs[i].set_xticks(heavy_fractions.index)
+
+
+def confidence_intervals_table(flux_model: FluxModel, model_fit: ModelFit, ci_level: float) -> pd.DataFrame:
+    fitted_net_fluxes = model_fit.model_state.flux_state.net_fluxes
+    net_flux_std_err = model_fit.fitted_fluxes_std_err()
+    ci_scale_factor = norm.isf((1 - ci_level)/2)
+    return pd.DataFrame(
+        {
+            'net': fitted_net_fluxes,
+            'std err': net_flux_std_err,
+            'ci_half-width': net_flux_std_err*ci_scale_factor,
+            'ci_lower': fitted_net_fluxes - net_flux_std_err*ci_scale_factor,
+            'ci_upper': fitted_net_fluxes + net_flux_std_err*ci_scale_factor,
+        },
+        index=pd.Index(flux_model.reactions, name='reaction')
+    )
